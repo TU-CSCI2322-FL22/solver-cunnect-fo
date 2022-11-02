5752d451744dbc4a659686e06874fc53dfc54df1
2021-08-24 <charger@janus19.cs.trinity.edu>
2022-11-02 13:05:46 -0500

    Updated types

--- types.hs
+++ types.hs
@@ -1,19 +1,23 @@
--- Put potential type representations here:
---
---
-
+import Data.Tuple (swap)
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import Debug.Trace
 
 data Player = Red | Yellow deriving Show
-data Piece = Player | Empty deriving Show
-data Board = [[Piece]] deriving Show
-data Turn = Player Row deriving Show
-
+data Piece = Full Player | Empty deriving Show
+type Board = [[Piece]] 
+type Move = Int 
+type GameState = (Board, Player)
+data Outcome = Winner Player | NoWinner | Tie
 
 
 
 getWinner :: Board -> Player
+getWinner = undefined
+makeMove :: Int -> GameState -> Maybe Board
+makeMove = undefined
+validMoves :: Board -> [Move]
+validMoves = undefined
 
-makeMove :: Board -> Board
-
-validMoves :: Board -> [Turn]
 
