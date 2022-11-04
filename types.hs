4d9f993d04fcedb2518b0ccd6e2ec333df65a484
2021-08-24 <charger@janus19.cs.trinity.edu>
2022-11-04 13:23:23 -0500

    Started makeMove

--- types.hs
+++ types.hs
@@ -15,8 +15,19 @@ data Outcome = Winner Player | NoWinner | Tie
 
 getWinner :: GameState -> Outcome
 getWinner = undefined
+
+makeRowMove :: Int -> [Piece] -> Player -> ([Player], Bool)
+makeRowMove 0 (x:xs) turn = 
+	if (x == Empty)
+    then ((Full turn):xs, True)
+    else (x:xs, False)
+makeRowMove col (x:xs) turn =
+	let (row, hasPlaced) = (makeRowMove (col - 1) xs turn)
+	in (x:row, hasPlaced)
+foo (row:rows)
 makeMove :: Int -> GameState -> Maybe Board
-makeMove col state = 
+makeMove col state = foo col (reverse state)
+	
 validMoves :: GameState -> [Move]
 validMoves = undefined
 
