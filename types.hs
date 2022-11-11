06548c115b44b10841d276ee2b4a740365fef8fd
2021-08-24 <bheston@janus16.cs.trinity.edu>
2022-11-11 13:23:11 -0600

    getWinner kinda updated but not working correctly

--- types.hs
+++ types.hs
@@ -9,26 +9,37 @@ data Piece = Full Player | Empty deriving (Show,Eq)
 type Board = [[Piece]] 
 type Move = Int 
 type GameState = (Board, Player)
-data Outcome = Winner Player | NoWinner | Tie
+data Outcome = Winner Player | NoWinner | Tie deriving (Show,Eq)
 
 
 
-getWinner :: Board -> Maybe Player
-getWinner brd = if(rows == Nothing) then (cols) else rows
-   where checkRow ((_:_:_:[]):_) = Nothing
-         checkRow ((a:b:c:d:xs):ys) = if(all (\p -> p == Full Red) [a,b,c,d]) then Just Red 
-                                 else if(all (\p -> p == Full Yellow) [a,b,c,d]) then Just Yellow
-                                 else checkRow ((b:c:d:xs):ys)
-        -- checkCol [[]] = Nothing
-         checkCol ([]:_:_:_:_) = Nothing
-         checkCol (_:[]:_:_:_) = Nothing
-         checkCol (_:_:[]:_:_) = Nothing
-         checkCol (_:_:_:[]:_) = Nothing
-         checkCol ((a:ws):(b:xs):(c:ys):(d:zs):ss) = if(all (\p -> p == Full Red) [a,b,c,d]) then Just Red
-                                 else if(all (\p -> p == Full Yellow) [a,b,c,d]) then Just Yellow
-                                 else checkCol (ws:xs:ys:zs:ss)
+getWinner :: GameState -> Outcome
+getWinner (brd,_) = if(rows == NoWinner) then (cols) else rows
+   where checkRow ((_:_:_:[]):[]) = NoWinner
+         checkRow ((_:_:_:[]):ys) = checkRow ys
+         checkRow ((a:b:c:d:xs):ys) = if(checkFour a b c d == NoWinner) then checkRow ((b:c:d:xs):ys)
+                                      else checkFour a b c d
+
+         checkCol brd = checkRow $ transpose brd
+         {-checkCol ([]:_:_:_:[]) = NoWinner
+         checkCol (_:[]:_:_:[]) = NoWinner
+         checkCol (_:_:[]:_:[]) = NoWinner
+         checkCol (_:_:_:[]:[]) = NoWinner
+         checkCol ([]:_:_:_:ys) = checkCol ys
+         checkCol (_:[]:_:_:ys) = checkCol ys
+         checkCol (_:_:[]:_:ys) = checkCol ys
+         checkCol (_:_:_:[]:ys) = checkCol ys
+         checkCol ((a:ws):(b:xs):(c:ys):(d:zs):ss) = if(checkFour a b c d == NoWinner) then checkCol (ws:xs:ys:zs:ss)
+                                                     else checkFour a b c d -}
+
+
          rows = checkRow brd
          cols = checkCol brd
+
+         checkFour a b c d = if(all (\p -> p == Full Red) [a,b,c,d]) then Winner Red
+                             else if(all (\p -> p == Full Yellow) [a,b,c,d]) then Winner Yellow
+							 else NoWinner
+
 
 
 makeMove :: Int -> GameState -> Maybe Board
